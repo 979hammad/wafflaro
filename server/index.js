940f696ab14dd94881a5805bf48e5ffc6a1128da
Hammad Akram <115222833+979hammad@users.noreply.github.com>
2024-01-24 00:18:42 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,46 +4,41 @@ import { dbConnection } from "./Database/Connection.js";
 import cookieParser from 'cookie-parser';
 import userRoutes from "./Routes/userRoutes.js";
 import itemRoutes from "./Routes/itemRoutes.js";
- import cors from "cors";
-
-const PORT = process.env.PORT || 8082;
-
+import cors from "cors"
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
 
-//Use cors middleware before defining routes
 app.use(cors(
     {
         origin : ['http://localhost:5173/'],
-        methods : ["POST", "GET"],
+        methods : ["POST", "GET", "DELETE"],
         credentials : true
     }
 ))
 
-// Set headers in response to handle CORS
 // app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   );
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//     );
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
-//   next();
+//     next();
 // });
 
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/items", itemRoutes);
 
-app.use((err, req, res, next) => {
-  let { status = 500, message = "Something went wrong" } = err;
-  return res.status(status).json({ message: message });
-});
+app.use((err, req, res, next) =>{
+    let {status = 500, message = "Something went wrong"} = err;
+    return res.status(status).json({message : message});
+})
 
-app.listen(PORT, () => {
-  dbConnection();
-  console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT , ()=>{
+    dbConnection();
+    console.log("Backend Connected!");
 });
