21528727c95e3d2e0707e1009cc8d7479278e3ec
Hammad Akram <115222833+979hammad@users.noreply.github.com>
2024-01-23 22:46:50 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,16 +19,16 @@ app.use(cors(
     }
 ))
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader(
-//         'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//     );
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
-//     next();
-// });
+    next();
+});
 
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/items", itemRoutes);
