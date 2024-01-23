286c32c702d1a61cbe737fecca5cfca87ef57c82
Hammad Akram <115222833+979hammad@users.noreply.github.com>
2024-01-23 23:02:37 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 // Use cors middleware before defining routes
 app.use(cors({
-  origin: 'https://wafflaro-frontend.vercel.app',
+  origin: "https://wafflaro-frontend.vercel.app",
   methods: ["POST", "GET", "DELETE"],
   credentials: true
 }));
