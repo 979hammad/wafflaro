25d4031db67f833ae3dde9aa7e80357389b96ad3
Hammad Akram <115222833+979hammad@users.noreply.github.com>
2024-01-24 08:24:48 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server : {
-    '/api' : 'https://wafflaro-server.vercel.app'
+    '/api' : 'http://localhost:8082'
   },
   plugins: [react()],
 })
